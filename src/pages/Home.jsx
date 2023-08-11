c4e5d66989bf4e0ab45ce42691dfc78d8c9360d4
majdiShomali <majdi.shomali.1997@gmail.com>
2023-08-11 11:39:03 +0300

    edit files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Home = () => {
+
+  const customKey = process.env.REACT_APP_CUSTOM_KEY;
+console.log(customKey); // Outputs the value defined in the .env file
   return (
     <div>Home</div>
   )
